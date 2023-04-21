5c0f1b23a339a0f0c46456315d612c31b7faa826
V_amerk <49831277+Vamerk@users.noreply.github.com>
2023-04-21 22:20:40 +0500

    21.04.23

--- SuccesBot/Person/__init__.py
+++ SuccesBot/Person/__init__.py
@@ -3,4 +3,3 @@ from .Generation_person import *
 from .Top_list import *
 from .PvP_agent import *
 from .Inventory import *
-from .exp import *
--- SuccesBot/other/lootbox.py
+++ SuccesBot/other/lootbox.py
@@ -1,12 +1,13 @@
 import sqlite3
 import random
 from PIL import Image
-from loader import dp, bot
+from Person import Inventory, Person
+
 
 
 async def send_lootbox(user_id):
     chance = random.randrange(0, 100)
-    if chance >= 50:
+    if chance >= 1:
         con = sqlite3.connect('gamebase.db')
         cur = con.cursor()
 
@@ -29,32 +30,58 @@ def open_standart_lootbox(user_id):
     con = sqlite3.connect('gamebase.db')
     cur = con.cursor()
 
-    gantel = random.randint(0, 3)
+    invent = Inventory(user_id)
+
+    pers = Person(user_id)
+    lvl = pers.Level()
+
+    gantel = random.randint(0, 2)
     path = 'Person_image/generate_image'
     cross = random.choices([f'{path}/cross-1.png', f'{path}/cross-2.png', f'{path}/cross-3.png'])[0]
     remen = random.choices([f'{path}/belt-0.png', f'{path}/belt-1.png'])[0]
     undp = random.choices([f'{path}/undp-cc.png', f'{path}/undp-gold.png', f'{path}/undp-russia.png'])[0]
-    loot = random.choices([gantel, cross, remen, undp], weights=[90, 2, 5, 3])[0]
+    preloot = [gantel]
+    ves = [90]
+    if invent.sneakers() == 0:
+        preloot.append(cross)
+        ves.append(2)
+    if invent.belt() == 0:
+        preloot.append(remen)
+        ves.append(5)
+    if invent.undp() == 0:
+        preloot.append(undp)
+        ves.append(3)
+    loot = random.choices(preloot, weights=ves)[0]
 
     if loot == gantel:
-        cur.execute(f"UPDATE inventory SET gantel=gantel+{loot}, lootboxs_s=lootboxs_s-1 WHERE id=?", (user_id,))
-        cur.execute(f"UPDATE gameinf SET stamina=stamina+{loot*0.05} WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE inventory SET gantel=gantel+{loot}, lootboxs_s=lootboxs_s WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE gameinf SET stamina=stamina+{loot * 0.05} WHERE id=?", (user_id,))
         con.commit()
         return f'{loot} гантелей'
 
     elif loot == cross:
+        cur.execute(f"UPDATE inventory SET cross=cross+1 WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE gameinf SET health=health+{2 * lvl + 5} WHERE id=?", (user_id,))
+        con.commit()
         update_pers_photo(user_id, cross)
         return '👟Кроссовки'
 
     elif loot == remen:
+        cur.execute(f"UPDATE inventory SET remen=remen+1 WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE gameinf SET stamina=stamina+{2 * lvl + 5} WHERE id=?", (user_id,))
+        con.commit()
         update_pers_photo(user_id, remen)
         return '🥇Пояс'
 
     elif loot == undp:
+        cur.execute(f"UPDATE inventory SET undp=undp+1 WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE gameinf SET stamina=stamina+{1.5 * lvl + 3}, health=health+{1.5 * lvl + 3}  WHERE id=?", (user_id,))
+        con.commit()
         update_pers_photo(user_id, undp)
         return '🩲Трусы)'
 
-    else: return 'Ничего((('
+    else:
+        return 'Ничего((('
 
     cur.close()
     con.close()
@@ -64,28 +91,55 @@ def open_premium_lootbox(user_id):
     con = sqlite3.connect('gamebase.db')
     cur = con.cursor()
 
-    gantel = random.randint(1, 5)
+    invent = Inventory(user_id)
+
+    pers = Person(user_id)
+    lvl = pers.Level()
+
+    gantel = random.randint(3, 10)
     path = 'Person_image/generate_image'
     cross = random.choices([f'{path}/cross-1.png', f'{path}/cross-2.png', f'{path}/cross-3.png'])[0]
     remen = random.choices([f'{path}/belt-0.png', f'{path}/belt-1.png'])[0]
     undp = random.choices([f'{path}/undp-cc.png', f'{path}/undp-gold.png', f'{path}/undp-russia.png'])[0]
-    loot = random.choices([gantel, cross, remen, undp], weights=[50, 12, 20, 18])[0]
+    preloot = [gantel]
+    ves = [50]
+    if invent.sneakers() == 0:
+        preloot.append(cross)
+        ves.append(12)
+    if invent.belt() == 0:
+        preloot.append(remen)
+        ves.append(20)
+    if invent.undp() == 0:
+        preloot.append(undp)
+        ves.append(18)
+    loot = random.choices(preloot, weights=ves)[0]
+
 
     if loot == gantel:
-        cur.execute(f"UPDATE inventory SET gantel=gantel+{loot}, lootboxs_p=lootboxs_p-1 WHERE id=?", (user_id,))
-        cur.execute(f"UPDATE gameinf SET stamina=stamina+{loot*0.05} WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE inventory SET gantel=gantel+{loot}, lootboxs_s=lootboxs_s WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE gameinf SET stamina=stamina+{loot * 0.05} WHERE id=?", (user_id,))
         con.commit()
         return f'{loot} гантелей'
 
     elif loot == cross:
+        cur.execute(f"UPDATE inventory SET cross=cross+1 WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE gameinf SET health=health+{2 * lvl + 5} WHERE id=?", (user_id,))
+        con.commit()
         update_pers_photo(user_id, cross)
         return '👟Кроссовки'
 
     elif loot == remen:
+        cur.execute(f"UPDATE inventory SET remen=remen+1 WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE gameinf SET stamina=stamina+{2 * lvl + 5} WHERE id=?", (user_id,))
+        con.commit()
         update_pers_photo(user_id, remen)
         return '🥇Пояс'
 
     elif loot == undp:
+        cur.execute(f"UPDATE inventory SET undp=undp+1 WHERE id=?", (user_id,))
+        cur.execute(f"UPDATE gameinf SET stamina=stamina+{1.5 * lvl + 3}, health=health+{1.5 * lvl + 3}  WHERE id=?",
+                    (user_id,))
+        con.commit()
         update_pers_photo(user_id, undp)
         return '🩲Трусы)'
 
